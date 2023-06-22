a278b8f43db183e3feff2812f9800a43359cf297
emdinga <emdinga@gmail.com>
2023-06-22 16:02:22 -0700

    update

--- push_pall.c
+++ push_pall.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * f_push - add node to the stack
+ * _push - add node to the stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_push(stack_t **head, unsigned int counter)
+void _push(stack_t **head, unsigned int counter)
 {
 	int n, j = 0, flag = 0;
 
@@ -34,25 +34,4 @@ void f_push(stack_t **head, unsigned int counter)
 		addnode(head, n);
 	else
 		addqueue(head, n);
-}
-#include "monty.h"
-/**
- * f_pall - prints the stack
- * @head: stack head
- * @counter: no used
- * Return: no return
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
-
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
 }
